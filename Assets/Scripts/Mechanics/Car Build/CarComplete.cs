f537906e216c9f530e3041de1b2230ce2e38694a
GDLinford <55755896+GDLinford@users.noreply.github.com>
2020-12-27 14:39:46 +0000

    All Unity Work

---
+++ Assets/Scripts/Mechanics/Car Build/CarComplete.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarComplete : MonoBehaviour
+{
+
+	public GameObject carEngine;
+
+	public GameObject carWheel, carWheel2, carWheel3, carWheel4;
+
+	public GameObject carGear;
+
+	public GameObject carSteeringWheel;
+
+	public bool carInRange;
+
+	public bool carComplete;
+
+	public bool hasEngine;
+
+	public bool hasGear;
+
+	public bool hasSteeringWheel;
+
+	public bool hasWheel;
+
+	public bool steeringWheelFitted;
+
+	public bool engineFitted;
+
+	public bool gearFitted;
+
+	public bool wheelFitted;
+
+	public void Update() {
+		ChangeScene();
+		if (engineFitted && gearFitted && steeringWheelFitted && wheelFitted) {
+			carComplete = true;
+			Debug.Log("We dun built a car son");
+		}
+	}
+	private void Start() {
+		carComplete = false;
+		hasEngine = false;
+		hasGear = false;
+		hasSteeringWheel = false;
+		hasWheel = false;
+		wheelFitted = false;
+		gearFitted = false;
+		engineFitted = false;
+		steeringWheelFitted = false;
+		carInRange = false;
+	}
+
+	public void LoadNextScene() {
+		SceneManager.LoadScene(SceneManager.sceneCount + 1);
+		Debug.Log("Scene Loaded");
+	}
+
+	public void ChangeScene() {
+		if (carComplete) {
+			LoadNextScene();
+		}
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		carInRange = true;
+	}
+
+	private void OnTriggerExit(Collider other) {
+		carInRange = false;
+	}
+
+
+	private void CarIsComplete() {
+		
+	}
+}
