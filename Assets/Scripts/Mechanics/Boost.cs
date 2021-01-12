9bb2b8d2091b0ed405541b7d6509307bf852268f
GDLinford <55755896+GDLinford@users.noreply.github.com>
2021-01-12 18:42:48 +0000

    begun powerups

---
+++ Assets/Scripts/Mechanics/Boost.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boost : MonoBehaviour
+{
+    [SerializeField]
+    float boost = 10f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Car")
+        {
+            Vector3 push = new Vector3(0, boost, 0f);
+            other.gameObject.GetComponent<Rigidbody>().AddForce(push);
+        }
+    }
+
+}
